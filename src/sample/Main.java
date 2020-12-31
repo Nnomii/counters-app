c083dca1cc9896419a02ddace9d652e4bc5ee2ee
xrobothd <xrobothd@gmail.com>
2020-12-31 17:06:52 +0100

    fixed my pc, added icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -17,6 +18,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("LOL Counters App");
         primaryStage.setScene(new Scene(root, 1000, 500));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         primaryStage.show();
     }
 
